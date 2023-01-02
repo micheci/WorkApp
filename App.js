5fc95f57710ade23e4967135dd66a0d5914d6709
micheci <michecimartinez@gmail.com>
2023-01-02 16:41:23 -0600

    added background color and more text

--- App.js
+++ App.js
@@ -1,20 +1,126 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,ScrollView ,Text, View,TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
 
 export default function App() {
   return (
+    <>
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ScrollView horizontal={true} >
+      <Text style={styles.Name}>Monday</Text>
+      <Text style={styles.Name}>Tuesday</Text>
+      <Text style={styles.Name}>Wednesday</Text>
+      <Text style={styles.Name}>Thursday</Text>
+      <Text style={styles.Name}>Friday</Text>
+      <Text style={styles.Name}>Saturday</Text>
+      <Text style={styles.Name}>Sunday</Text>
+      <Text >Sunday</Text>
+      </ScrollView>
+      
     </View>
+     <View style={styles.container}>
+     <StatusBar style="auto" />
+     <ScrollView horizontal={true} >
+     <Text style={styles.Name}>Monday</Text>
+     <Text style={styles.Name}>Tuesday</Text>
+     <Text style={styles.Name}>Wednesday</Text>
+     <Text style={styles.Name}>Thursday</Text>
+     <Text style={styles.Name}>Friday</Text>
+     <Text style={styles.Name}>Saturday</Text>
+     <Text style={styles.Name}>Sunday</Text>
+     <Text >Sunday</Text>
+     </ScrollView>
+     
+   </View>
+   <View style={styles.container}>
+     <StatusBar style="auto" />
+     <ScrollView horizontal={true} >
+     <Text style={styles.Name}>Monday</Text>
+     <Text style={styles.Name}>Tuesday</Text>
+     <Text style={styles.Name}>Wednesday</Text>
+     <Text style={styles.Name}>Thursday</Text>
+     <Text style={styles.Name}>Friday</Text>
+     <Text style={styles.Name}>Saturday</Text>
+     <Text style={styles.Name}>Sunday</Text>
+     <Text >Sunday</Text>
+     </ScrollView>
+     
+   </View>
+   <View style={styles.container}>
+     <StatusBar style="auto" />
+     <ScrollView horizontal={true} >
+     <Text style={styles.Name}>Monday</Text>
+     <Text style={styles.Name}>Tuesday</Text>
+     <Text style={styles.Name}>Wednesday</Text>
+     <Text style={styles.Name}>Thursday</Text>
+     <Text style={styles.Name}>Friday</Text>
+     <Text style={styles.Name}>Saturday</Text>
+     <Text style={styles.Name}>Sunday</Text>
+     <Text >Sunday</Text>
+     </ScrollView>
+     
+   </View>
+   <View style={styles.container}>
+     <StatusBar style="auto" />
+     <ScrollView horizontal={true} >
+     <Text style={styles.Name}>Monday</Text>
+     <Text style={styles.Name}>Tuesday</Text>
+     <Text style={styles.Name}>Wednesday</Text>
+     <Text style={styles.Name}>Thursday</Text>
+     <Text style={styles.Name}>Friday</Text>
+     <Text style={styles.Name}>Saturday</Text>
+     <Text style={styles.Name}>Sunday</Text>
+     <Text >Sunday</Text>
+     </ScrollView>
+     
+   </View>
+   <View style={styles.container}>
+     <StatusBar style="auto" />
+     <ScrollView horizontal={true} >
+     <Text style={styles.Name}>Monday</Text>
+     <Text style={styles.Name}>Tuesday</Text>
+     <Text style={styles.Name}>Wednesday</Text>
+     <Text style={styles.Name}>Thursday</Text>
+     <Text style={styles.Name}>Friday</Text>
+     <Text style={styles.Name}>Saturday</Text>
+     <Text style={styles.Name}>Sunday</Text>
+     <Text >Sunday</Text>
+     </ScrollView>
+     
+   </View>
+   <View style={styles.container}>
+     <StatusBar style="auto" />
+     <ScrollView horizontal={true} >
+     <Text style={styles.Name}>Monday</Text>
+     <Text style={styles.Name}>Tuesday</Text>
+     <Text style={styles.Name}>Wednesday</Text>
+     <Text style={styles.Name}>Thursday</Text>
+     <Text style={styles.Name}>Friday</Text>
+     <Text style={styles.Name}>Saturday</Text>
+     <Text style={styles.Name}>Sunday</Text>
+     <Text >Sunday</Text>
+     </ScrollView>
+     
+   </View>
+   </ScrollView>  
+   </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'gray',
+    alignItems: 'flex-start',
     justifyContent: 'center',
+    flexDirection:'row',
+    marginTop: 60,
+    height: 100,
+    
+  },
+  Name:{
+    color:'blue',
   },
 });
